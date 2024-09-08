f387b09ad72b7db9f28fdc7accf0927ec7da61ee
Zihou <wongzihou1997@gmail.com>
2024-09-09 02:58:58 +0800

    git init

---
+++ Sync.sh
@@ -0,0 +1,195 @@
+# USAGE:
+# chmod -x Sync.sh
+# ./Sync.sh
+
+echo "Setting Up For Zihou's MacOS ..."
+
+# Homebrew
+if test ! $(which omz); then
+    echo "Instlling Homebrew"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+# Oh My Zsh
+
+
+if test ! $(which omz); then
+    echo "Installing Oh My Zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# Fixing oh my zsh write premission
+echo "Fixing oh my zsh write permission"
+compaudit | xargs chmod g-w,o-w
+
+# Brew update && upgrade
+echo "Brew update && upgrade"
+brew update
+brew upgrade
+
+# Application
+echo "Installing Applications"
+brew install # Application
+echo "Instlling Applications"
+brew install --cask proxyman
+brew install --cask lookin
+brew install --cask fork
+brew install --cask spotify
+brew install --cask wechat
+brew install --cask dingtalk
+brew install --cask macdown
+
+
+# iTerm2
+echo "Instlling iTerm2"
+brew install --cask iterm2
+
+
+# Oh My Zsh
+echo "Instlling Oh My Zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# fix oh my zsh write permission
+echo "fixing oh my zsh write permission"
+compaudit | xargs chmod g-w,o-w
+
+# brew update && upgrade
+echo "brew update && upgrade"
+brew update
+brew upgrade
+
+# Application
+echo "Instlling Applications"
+brew install --cask proxyman
+brew install --cask lookin
+brew install --cask fork
+brew install --cask spotify
+brew install --cask wechat
+brew install --cask dingtalk
+brew install --cask macdown
+brew install bear
+
+# Work Tools
+echo "Instlling Work Tools"
+brew install git
+brew install wget
+brew install jenv
+brew install rbenv
+
+brew install openssl
+brew install pyenv
+pyenv install 3.7.10
+pyenv global 3.7.10 system
+
+# set iTerm2 font
+# Meslo LG M DZ for Powerline
+echo "Setting iTerm2 font"
+git clone https://github.com/powerline/fonts.git --depth=1
+./fonts/install.sh
+rm -rf fonts
+# set iTerm2 font manually
+
+# Xcode
+echo "Instlling Xcode Tools"
+brew install cocoapods
+brew install xcodegen
+brew install swiftformat
+
+# brew cleanup
+echo "brew cleanup"
+brew cleanup
+
+# brew doctor
+echo "brew doctor"
+brew doctor
+
+source ~/.zshrc
+
+echo "Setting Up..."
+
+# Oh My Zsh
+echo "Installing Oh My Zsh"
+if test ! $(which omz); then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# Fixing oh my zsh write premission
+echo "Fixing oh my zsh write permission"
+compaudit | xargs chmod g-w,o-w
+
+# Brew update && upgrade
+echo "Brew update && upgrade"
+brew update
+brew upgrade
+
+# Application
+echo "Installing Applications"
+brew install # Application
+echo "Instlling Applications"
+brew install --cask proxyman
+brew install --cask lookin
+brew install --cask fork
+brew install --cask spotify
+brew install --cask wechat
+brew install --cask dingtalk
+brew install --cask macdown
+
+
+# Homebrew
+echo "Instlling Homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# iTerm2
+echo "Instlling iTerm2"
+brew install --cask iterm2
+
+
+# Oh My Zsh
+echo "Instlling Oh My Zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# fix oh my zsh write permission
+echo "fixing oh my zsh write permission"
+compaudit | xargs chmod g-w,o-w
+
+# brew update && upgrade
+echo "brew update && upgrade"
+brew update
+brew upgrade
+
+# Application
+echo "Instlling Applications"
+brew install --cask proxyman
+brew install --cask lookin
+brew install --cask fork
+brew install --cask spotify
+brew install --cask wechat
+brew install --cask dingtalk
+brew install --cask macdown
+
+# Work Tools
+echo "Instlling Work Tools"
+brew install git
+brew install wget
+brew install jenv
+brew install rbenv
+
+brew install openssl
+brew install pyenv
+pyenv install 3.7.10
+pyenv global 3.7.10 system
+
+# Xcode
+echo "Instlling Xcode Tools"
+brew install cocoapods
+brew install xcodegen
+brew install swiftformat
+
+# brew cleanup
+echo "brew cleanup"
+brew cleanup
+
+# brew doctor
+echo "brew doctor"
+brew doctor
+
+source ~/.zshrc
---
+++ mac_auto_init.sh
@@ -0,0 +1,12 @@
+# 创建一个列表，包含需要安装的应用
+homebrew_apps=("python" "git" "tree")
+
+# 遍历应用列表，检查每个应用是否已经安装，如果没有则异步安装
+for app in "${homebrew_apps[@]}"; do
+    if ! brew ls --versions "$app" >/dev/null; then
+        echo "$app 应用未安装，正在异步安装..."
+        brew install "$app" &
+    else
+        echo "$app 应用已经安装！"
+    fi
+done
